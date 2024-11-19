a2c8d74088d54a80009346d4ff42084415bcea59
ugi85 <ugijunkies@gmail.com>
2024-11-19 22:15:05 +0700

    first commit

---
+++ wp-content/plugins/elementor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/elementor',
+        'pretty_version' => '3.25.x-dev',
+        'version' => '3.25.9999999.9999999-dev',
+        'reference' => '5c7d85bfded98ea15c6c1ae77b035f93619cb765',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/elementor' => array(
+            'pretty_version' => '3.25.x-dev',
+            'version' => '3.25.9999999.9999999-dev',
+            'reference' => '5c7d85bfded98ea15c6c1ae77b035f93619cb765',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
